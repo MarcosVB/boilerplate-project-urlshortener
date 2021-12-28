64f467d126e5f5183cd9445dbed9ebd8c84c684a
Marcos Vinicius Berté <mvberte@hotmail.com>
2021-12-28 17:56:53 -0300

    URL Shortener Microservice solution

--- server.js
+++ server.js
@@ -1,24 +1,52 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/shorturl", (req, res) => {
+  if (!/https?:\/\/(?:www\.)?.+\..+(?:\/.*)?/.test(req.body.url)) {
+    res.json({ error: "invalid url" });
+    return;
+  }
+
+  const urlObject = postUrl(req.body.url);
+  res.json(urlObject);
+});
+
+app.get("/api/shorturl/:shortUrl", (req, res) => {
+  res.redirect(urlDB[req.params.shortUrl].original_url);
+});
+
+const urlDB = [];
+
+function postUrl(url) {
+  const shortUrl = urlDB.length;
+  urlDB.push({
+    original_url: url,
+    short_url: shortUrl
+  });
+  return urlDB[shortUrl];
+}
